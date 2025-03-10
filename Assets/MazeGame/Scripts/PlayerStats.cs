59d4085a6f5344ffa7cc5fc25051cf331401ee1c
Owens-D10 <Owens-D10@ulster.ac.uk>
2025-03-10 01:40:28 +0000

    Added jumpscare, fixed enemy states

--- Assets/MazeGame/Scripts/PlayerStats.cs
+++ Assets/MazeGame/Scripts/PlayerStats.cs
@@ -1,9 +1,11 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.Android;
 
 public class PlayerStats : MonoBehaviour
 {
     public int collectables;
+    public GameObject jumpscare;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,5 +26,16 @@ public class PlayerStats : MonoBehaviour
             Destroy(other.gameObject);
             collectables = +1;
         }
+
+        if (other.tag == "Monster")
+        {
+            JumpScare();
+        }
+    }
+
+    private void JumpScare()
+    {
+        Time.timeScale = 0;
+        jumpscare.SetActive(true);
     }
 }
--- Assets/z_BaseFiles/Starter Assets/Runtime/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/z_BaseFiles/Starter Assets/Runtime/FirstPersonController/Scripts/FirstPersonController.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using UnityEditor.SearchService;
+using UnityEngine;
 #if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
 #endif
+using UnityEngine.SceneManagement;
 
 namespace StarterAssets
 {
@@ -68,7 +70,6 @@ namespace StarterAssets
 		private CharacterController _controller;
 		private StarterAssetsInputs _input;
 		private GameObject _mainCamera;
-
 		private const float _threshold = 0.01f;
 
 		private bool IsCurrentDeviceMouse
@@ -104,12 +105,15 @@ namespace StarterAssets
 
 			// reset our timeouts on start
 			_fallTimeoutDelta = FallTimeout;
+
+			
 		}
 
 		private void Update()
 		{
 			GroundedCheck();
 			Move();
+			
 		}
 
 		private void LateUpdate()
@@ -213,5 +217,7 @@ namespace StarterAssets
 			// when selected, draw a gizmo in the position of, and matching radius of, the grounded collider
 			Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z), GroundedRadius);
 		}
+
+		
 	}
 }
